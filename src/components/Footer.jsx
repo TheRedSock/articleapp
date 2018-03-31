9c0941ba90e88ce978a6a9a9f0d2d426871d2473
TheRedSock <torstein.rosok@gmail.com>
2018-03-31 15:50:09 +0200

    changed from function to a const

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
-function Footer() {
-  return (
-    <div>
-      <footer>
-        <div>
-          <p>Torstein Røsok &copy; 2018</p>
-          <p><a href="#root">Tilbake til toppen</a></p>
-        </div>
-      </footer>
-    </div>
-  );
-}
+const Footer = () => (
+  <div>
+    <footer>
+      <div>
+        <p>Torstein Røsok &copy; 2018</p>
+        <p><a href="#root">Tilbake til toppen</a></p>
+      </div>
+    </footer>
+  </div>
+);
 
 export default Footer;
